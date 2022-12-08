204e921a11ed4bc625b42c49882b38ad5cd8759c
Ivan <i.klimenko.eng@gmail.com>
2022-12-08 20:51:25 +0400

    Task_01 added

---
+++ Seminars/Seminar_1/Home_work_1/Task_01.java
@@ -0,0 +1,41 @@
+package Home_work_1;
+
+import java.util.Scanner;
+
+// Задача: Вычислить n-ое треугольного число(сумма чисел от 1 до n), 
+// n! (произведение чисел от 1 до n)
+public class Task_01 {
+    static int find_sum_num(int num) {
+        int sum = 0;
+        for (int i = 0; i <= num; i++)
+            sum += i;
+        return sum;
+    }
+
+    static int factorial(int num) {
+        int fact = 1;
+
+        if (num == 1 || num == 0)
+            return fact;
+        else {
+            for (int i = 0; i < num - 1; i++) {
+                fact += fact * (i + 1);
+            }
+            return fact;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число n: ");
+        int n = scanner.nextInt();
+        int result = find_sum_num(n);
+        System.out.println("n-ое треугольное число = " + result);
+        n = scanner.nextInt();
+        int factorial = factorial(n);
+        System.out.println("!" + n + " = " + factorial);
+        scanner.close();
+    }
+
+}
