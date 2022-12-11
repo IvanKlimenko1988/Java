041001f624c2f6aaeaefc8816a1ef05c75a45ea1
Ivan <i.klimenko.eng@gmail.com>
2022-12-11 13:44:08 +0300

    Task_01 and Task_02 added

--- Seminars/Seminar_2/Home_work_2/Task_01.java
+++ Seminars/Seminar_2/Home_work_2/Task_01.java
@@ -1,9 +1,30 @@
 package Seminars.Seminar_2.Home_work_2;
 // Задача 1:
+
 // Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder. 
 // Данные для фильтрации приведены ниже в виде json строки.
 // Если значение null, то параметр не должен попадать в запрос.
 // Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+// select * from Students where name = 'Ivanov' and country = 'Russia' and Ciry = 'Moscow' and age = null
+
+import java.util.HashMap;
+
 public class Task_01 {
-    // select * from Students where name = 'Ivanov' and country = 'Russia' and Ciry = 'Moscow' and age = null
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name", "Ivanov");
+        map.put("country", "Russia");
+        map.put("city", "Moscow");
+        map.put("age", null);
+
+        StringBuilder sb = new StringBuilder();
+        for (HashMap.Entry<String, String> pair : map.entrySet()) {
+            if (pair.getValue() == null)
+                continue;
+            sb.append(pair.getKey()).append(" = '").append(pair.getValue()).append("' and ");
+        }
+        sb.delete(sb.length() - 5, sb.length());
+        System.out.println(sb);
+    }
+
 }
--- Seminars/Seminar_2/Home_work_2/Task_02.java
+++ Seminars/Seminar_2/Home_work_2/Task_02.java
@@ -1,26 +1,29 @@
 package Seminars.Seminar_2.Home_work_2;
 
-import java.security.Principal;
-import java.util.PrimitiveIterator;
-
+import java.io.FileWriter;
+import java.io.IOException;
 // Задача 2:
 // Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
-// {1,6,3,7,2,5,4,8,10,9}
-// 1 3 6 2 5 4 7 8 9 10 
+
 public class Task_02 {
     public static void main(String[] args) {
-        int[] array = {9,6,3,7,2,5,4,8,10,9};
-        for(int i = 0; i < array.length; i++){
-            for(int j = 0; j < array.length-1; j++){
-                if(array[j] > array[j+1]){
-                    int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+        try (FileWriter writer = new FileWriter("log_bubble.txt", false)) {
+            int[] array = { 9, 6, 4, 62, 12, 10, 23, 8, 1, 2 };
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array.length - 1; j++) {
+                    if (array[j] > array[j + 1]) {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+                for (int j : array) writer.append(Integer.toString(j) + " ");
+                writer.append('\n');
+                writer.flush();
             }
-            }
-        }
-        for (int i = 0; i < array.length; i++) {
-            System.out.printf("%d ", array[i]);
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
         }
     }
 }
